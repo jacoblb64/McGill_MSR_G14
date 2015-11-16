5ae057ea44c59b0c56e96e6accef61263ac8b50e
cgathuru <charlesgathuru@gmail.com>
2015-11-16 07:01:18 -0500

    [task] Added git metrics to add commit length
    Added script git metrics to loop through the data dump csv from commit
    guru and parse the data for the commit hashes. Using each of the commit
    hashes it gets the length of the commit messages. From here it filters all
    the rows in the original data dump that don't have a valid commit hash so
    that the number of valid rows is equal to the size of the commit lengths
    list i.e. there is a commit length per commit hash. From here it appends
    the  new commit length data onto every row of data and writes out a new
    csv containing the previous information and the new commit length
    column.

---
+++ github_scraper/git_metrics.py
@@ -0,0 +1,65 @@
+import csv
+import re
+import subprocess
+import os
+
+__author__ = 'Charles'
+
+
+def main():
+    with open('compass2.csv') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        with open('results.csv', mode='w') as csv_file_out:
+            fieldnames = csv_reader.fieldnames[0:27]
+            commits = [commit for commit in csv_reader if re.search("^[a-z0-9]+$", commit['commit_hash'])]
+            commit_hashes = [commit_hash['commit_hash'] for commit_hash in commits]
+            print(commit_hashes)
+            commit_lengths = [get_commit_length(commit_hash) for commit_hash in commit_hashes]
+            print("Commit hashes: {} commit lengths {}", format(len(commit_hashes)), len(commit_lengths))
+            fieldnames.append('commit_length')
+            print(commit_lengths)
+            commits = [update_dict(commit, commit_lengths, index)
+                       for index, commit in enumerate(commits, start=0)]
+            print(fieldnames)
+            writer = csv.DictWriter(csv_file_out, fieldnames)
+            writer.writeheader()
+            commits = [filter_commits(commit, fieldnames) for commit in commits]
+            writer.writerows(commits)
+
+
+def filter_commits(commit, fieldnames):
+    return {key: commit[key] for key in commit if fieldnames.__contains__(key)}
+
+
+def update_dict(commit, commit_lengths, index):
+    commit.update({'commit_length': commit_lengths[index]})
+    return commit
+
+
+def get_commit_length(commit_hash):
+    os.chdir('compass2')
+    git_commit_fields = ['id', 'author_name', 'date', 'message_header', 'message_body', 'extra']
+    git_log_format = ['%H', '%an', '%ad', '%s', '%b']
+    git_log_format = '%x1e' + '%x1f'.join(git_log_format) + '%x1f'
+    options = ' show --format="%s" {}'.format(commit_hash)
+    command = 'git' + options
+
+    p = subprocess.Popen(command % git_log_format, shell=True, stdout=subprocess.PIPE)
+    (log, _) = p.communicate()
+    log = log.decode(encoding='ISO-8859-1')
+    log = log.strip('\n\x1e').split("\x1e")
+    log = (row.strip().split("\x1f") for row in log)
+    log = (dict(zip(git_commit_fields, row)) for row in log)
+    os.chdir('..')
+    for commit in log:
+        if not commit.__contains__('message_header'):
+            return -1
+        msg_bdy = commit.__contains__('message_body')
+        if msg_bdy:
+            return len(commit['message_header']) + len(commit['message_body'])
+        else:
+            return len(commit['message_header'])
+
+
+if __name__ == '__main__':
+    main()
