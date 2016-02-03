706b8631b35f2bfdc6a70686e75079de228af92f
Charles Gathuru <charlesgathuru@gmail.com>
2016-02-02 21:40:04 -0500

    [task] Update script to use parse csv's in dir
    Updated script to allow for the parsing of single files as well
    as all .csv files in a given directory. The output is then written
    to a file that that is specified and if none is, a generic filename
    merged_results will be used.
    
    A producer consumer model is empolyed where the multiple threads
    open and parse each of the files separtely and then post the processed
    data into a queue. A consumer process then takes items from the queue
    and writes them to a single file to imporce speed.

--- github_scraper/git_metrics.py
+++ github_scraper/git_metrics.py
@@ -6,6 +6,7 @@ import os
 import argparse
 import ntpath
 import concurrent.futures
+import multiprocessing
 
 __author__ = 'Charles'
 
@@ -23,70 +24,126 @@ while decrement:
     try:
         csv.field_size_limit(maxInt)
     except OverflowError:
-        maxInt = int(maxInt/10)
+        maxInt = int(maxInt / 10)
         decrement = True
+
 
 # Parallel 113.13 seconds
 # Parallel + write
 # Sequential time 172.2 seconds
 # Sequential + write 97.257 seconds
 
+fieldnames = ['commit_hash', 'commit_message', 'fix', 'classification', 'contains_bug', 'ns',
+              'nd', 'nf', 'entrophy', 'la', 'ld', 'ndev', 'age', 'nuc', 'exp', 'rexp', 'sexp',
+              'glm_probability', 'project_name', 'commit_words', 'commit_structure']
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', default='.', dest='project_path', type=str,
                         metavar='/home/Downloads/', help='project folder name')
-    parser.add_argument('-f', '--filename', dest='csv_path', required=True, metavar='data.csv',
+    source = parser.add_mutually_exclusive_group(required=True)
+    source.add_argument('-f', '--filename', dest='csv_path', metavar='data.csv',
                         type=str, help='csv file including path')
+    source.add_argument('-d', '--directory', type=str, metavar='/home/user/Downloads/',
+                        help='The directory containing the csv files')
+    parser.add_argument('-o', '--output', type=str, metavar='results.csv',
+                        default='results.csv', help='The output filename')
 
     args = parser.parse_args()
+
     project_path = args.project_path
     csv_path = args.csv_path
 
-    commit_struct_pat = re.compile('^(.{,20})((\[)|(fix)|(close)|(#\d+)|(sign)|(release)|(upgrade)|(bug)|(version)|'
-                                   '(id))',
-                                   re.IGNORECASE | re.MULTILINE)
+    files = []
 
-    files = list()
-    files.append(csv_path)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        for file in files:
-            # Do some work here
-            executor.submit(parse_file, file, project_path, commit_struct_pat)
-        executor.shutdown(wait=True)
+    if args.directory:
+        # The directory option was picked
+        project_name = 'merged'
+        print('Looking at directory')
+        for filename in os.listdir(args.directory):
+            if filename.endswith('.csv'):
+                files.append(os.path.join(args.directory, filename))
+
+    else:
+        print('Looking at single file')
+        files.append(csv_path)
+        project_name = os.path.splitext(ntpath.basename(csv_path))[0]
+
+    if args.output != 'results.csv':
+        out_file = args.output
+    else:
+        out_file = project_name + '_results.csv'
+
+    if project_path is not '.':
+        out_file = os.path.join(project_path, out_file)
+
+    print('Writing output to ' + out_file)
+
+    set_up(out_file, files)
+
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+    # for file in files:
+    # Do some work here
+    # executor.submit(parse_file, file, project_path, commit_struct_pat)
+    # executor.shutdown(wait=True)
     # parse_file(csv_path, project_path, commit_struct_pat)
 
 
-def parse_file(csv_path, project_path, commit_struct_pat):
-    print(csv_path)
-    print(project_path)
-    print(commit_struct_pat)
-    with open(csv_path) as csv_file:
+def parse_file(filename, commit_struct_pat, queue: multiprocessing.Queue):
+    project_name = os.path.splitext(ntpath.basename(filename))[0]
+    with open(filename) as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        print(os.path.splitext(ntpath.basename(csv_path))[0])
-        if project_path is not '.':
-            out_file = os.path.join(project_path, (os.path.splitext(ntpath.basename(csv_path))[0] + '_results.csv'))
+        commits = []
+        for commit in csv_reader:
+            if re.search("^[a-z0-9]+$", commit['commit_hash']):
+                commit_words = len(re.findall(r'\w+', commit['commit_message']))
+                commit_structure = False
+                if re.search(commit_struct_pat, commit['commit_message']):
+                    commit_structure = True
+                commit.update({'commit_words': commit_words})
+                commit.update({'commit_structure': commit_structure})
+                commit.update({'project_name': project_name})
+                commit = {key: commit[key] for key in commit if key in fieldnames}
+                commits.append(commit)
         else:
-            out_file = os.path.splitext(csv_path)[0] + '_results.csv'
-        print(out_file)
-        with open(out_file, mode='w', newline='') as csv_file_out:
-            fieldnames = list()
-            fieldnames.append(csv_reader.fieldnames[0])
-            fieldnames += csv_reader.fieldnames[5:8] + csv_reader.fieldnames[9:27]
-            fieldnames.append('commit_words')
-            fieldnames.append('commit_structure')
-            writer = csv.DictWriter(csv_file_out, fieldnames)
-            writer.writeheader()
-            for commit in csv_reader:
-                if re.search("^[a-z0-9]+$", commit['commit_hash']):
-                    commit_words = len(re.findall(r'\w+', commit['commit_message']))
-                    commit_structure = False
-                    if re.search(commit_struct_pat, commit['commit_message']):
-                        commit_structure = True
-                    commit.update({'commit_words': commit_words})
-                    commit.update({'commit_structure': commit_structure})
-                    commit = {key: commit[key] for key in commit if key in fieldnames}
-                    writer.writerow(commit)
+            queue.put(list(commits))
+
+
+def file_writer(dest_filename, queue, token):
+    with open(dest_filename, mode='w', newline='') as dest_file:
+
+        writer = csv.DictWriter(dest_file, fieldnames)
+        writer.writeheader()
+        while True:
+            data = queue.get()
+            if data == token:
+                return
+            for commit in data:
+                writer.writerow(commit)
+
+
+def set_up(outfile: str, files: list):
+    commit_struct_pat = re.compile('^(.{,20})((\[)|(fix)|(close)|(#\d+)|(sign)|(release)|(upgrade)|(bug)|(version)|'
+                                   '(id))', re.IGNORECASE | re.MULTILINE)
+    queue = multiprocessing.Queue()
+
+    stop_token = ''
+    writer_process = multiprocessing.Process(target=file_writer, args=(outfile, queue, stop_token))
+    writer_process.start()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        for file in files:
+            # Do some work here
+            executor.submit(parse_file, file, commit_struct_pat, queue)
+        executor.shutdown(wait=True)
+
+    # Dispatch jobs
+
+    # Make sure finished
+
+    queue.put(stop_token)
+    writer_process.join()
 
 
 def get_commit_length(commit_hash, project_path):
