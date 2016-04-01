f4c998c56c3c5bc55b3d0e4ad800c4e183740b16
Charles Gathuru <charlesgathuru@gmail.com>
2016-04-01 17:29:22 -0400

    started on performance

---
+++ analysis scripts/predictive_power.r
@@ -0,0 +1,48 @@
+require(taRifx)
+
+# removes metadata and converts all data to numerical
+cleanTable <- function(table) {
+  table$X <- NULL
+  table$name <- NULL
+  # table <- japply( table, which(sapply(table, class)=="logical"), as.numeric )
+
+  table <- table[rowSums(is.na(table))<1200,]
+
+  return(table)
+}
+
+process <- function(performance_with, performance_wo) {
+
+  name <- performance_with$name
+
+  performance_with <- cleanTable(performance_with)
+  performance_wo <- cleanTable(performance_wo)
+
+  # withMeans = rowMeans(performance_with, na.rm = TRUE, dims = 1)
+  # woMeans = rowMeans(performance_wo, na.rm = TRUE, dims = 1)
+
+  results <- data.frame(name)
+  results[,'Pwilcox'] <- NA
+
+  width <- length(names(performance_with))
+
+  pb <- txtProgressBar(min = 0, max = width, style = 3)
+
+  for (i in 1:width) {
+
+    trycatch({
+
+      wilcox <- wilcox.test(as.numeric(performance_with[i,]),
+                            as.numeric(performance_wo[i,3]),
+                            na.action=na.exclude)
+      results[i, 'Pwilcox'] <- as.numeric(wilcox[['p.value']])
+
+    }, error = function(e) {
+      print(paste("failed on", i))
+    })
+
+    setTxtProgressBar(pb, i)
+  }
+
+  return(results)
+}
